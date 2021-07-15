48a9f32b0bd3e77d9bd12486fc6e633b69a7f97d
Skarfe <gratskyiyurii@mail.ru>
2021-07-15 10:38:31 +0300

    Added Repository

--- DomainLayer/Interfaces/IBirthdayRepository.cs
+++ DomainLayer/Interfaces/IBirthdayRepository.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainLayer.Entities;
 
 namespace DomainLayer.Interfaces
 {
-    public class IBirthdayRepository
+    public interface IBirthdayRepository
     {
-
+        public void CreateBirthday(Birthday birthday);
+        public void UpdateBirthday(Birthday birthday);
+        public void RemoveBirthday(Birthday birthday);
+        public List<Birthday> GetBirthdays();
+        public List<Birthday> GetUpcommingBirthdays();
     }
 }
--- Services/Persistance/BirthdayRepository.cs
+++ Services/Persistance/BirthdayRepository.cs
@@ -17,10 +17,34 @@ namespace Services.Persistance
             _context = context;
         }
 
+        public void CreateBirthday(Birthday birthday)
+        {
+            _context.Birthdays.Add(birthday);
+            _context.SaveChanges();
+        }
+
+        public void UpdateBirthday(Birthday birthday)
+        {
+            _context.Birthdays.Update(birthday);
+            _context.SaveChanges();
+        }
+
+        public void RemoveBirthday(Birthday birthday)
+        {
+            _context.Birthdays.Remove(birthday);
+            _context.SaveChanges();
+        }
+
         public List<Birthday> GetBirthdays()
         {
             List<Birthday> list = _context.Birthdays.ToList();
+            return list;
+        }
 
+        public List<Birthday> GetUpcommingBirthdays()
+        {
+            List<Birthday> list = _context.Birthdays.Where(b => (b.BirthdayTime.Month == DateTime.Now.Month && b.BirthdayTime.Day >= DateTime.Now.Day) || 
+                (b.BirthdayTime.Month == DateTime.Now.AddMonths(1).Month && b.BirthdayTime.Day <= DateTime.Now.Day)).ToList();
             return list;
         }
     }
